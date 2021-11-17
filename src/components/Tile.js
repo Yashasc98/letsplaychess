a12cf8d17241624095134706a6bf922459322932
Yashasc98 <yashas998@gmail.com>
2021-11-17 16:49:36 +0530

    cursor changes on tiles and chessboard

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -9,14 +9,23 @@ class Tile extends React.Component {
     }
 
     render() {
-        if((this.props.xAxis + this.props.yAxis) % 2 === 0){
-            return <div className="tile light-tile">
-                <div className="piece" style={{backgroundImage: `url('${process.env.PUBLIC_URL}/assets/pieces/${positionPieceMap[`${this.props.xAxis}${this.props.yAxis}`]}')`}}></div>
-            </div>;
-        }else {
-            return <div className="tile dark-tile">
-                <div className="piece" style={{backgroundImage: `url('${process.env.PUBLIC_URL}/assets/pieces/${positionPieceMap[`${this.props.xAxis}${this.props.yAxis}`]}')`}}></div>
-            </div>;
+        if(positionPieceMap[`${this.props.xAxis}${this.props.yAxis}`] !== undefined){
+            if((this.props.xAxis + this.props.yAxis) % 2 === 0){
+                return <div className="tile light-tile">
+                    <div className="piece" style={{backgroundImage: `url('${process.env.PUBLIC_URL}/assets/pieces/${positionPieceMap[`${this.props.xAxis}${this.props.yAxis}`]}')`}}></div>
+                </div>;
+            }else {
+                return <div className="tile dark-tile">
+                    <div className="piece" style={{backgroundImage: `url('${process.env.PUBLIC_URL}/assets/pieces/${positionPieceMap[`${this.props.xAxis}${this.props.yAxis}`]}')`}}></div>
+                </div>;
+            }
+        }
+        else{
+            if((this.props.xAxis + this.props.yAxis) % 2 === 0){
+                return <div className="tile light-tile"></div>;
+            }else {
+                return <div className="tile dark-tile"></div>;
+            }
         }
     }
 } 
