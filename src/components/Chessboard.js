f863eabe0dae821edc8d0fa43cc46c848b5b112c
Yashasc98 <yashas998@gmail.com>
2021-11-17 19:00:58 +0530

    right click removes piece. few extra movement related changes

--- src/components/Chessboard.js
+++ src/components/Chessboard.js
@@ -7,33 +7,35 @@ class Chessboard extends React.Component {
     constructor(props) {
         super(props);
         this.state = { 
-                        width: window.innerWidth, 
-                        height: window.innerHeight,
                         grabbedPiece: null
                     }
         this.grabPiece = this.grabPiece.bind(this);
         this.movePiece = this.movePiece.bind(this);
         this.dropPiece = this.dropPiece.bind(this);
+        this.handleRightClick = this.handleRightClick.bind(this);
     }
 
     grabPiece(e) {
-        if(e.target.classList.contains("piece")){
-            const mouseX = e.clientX - (e.target.clientWidth/2);
-            const mouseY = e.clientY - (e.target.clientHeight/2);
-            e.target.style.position = "absolute";
-            e.target.style.left = `${mouseX}px`;
-            e.target.style.top = `${mouseY}px`;
-            this.setState({
-                grabbedPiece: e
-            })
+        if(e.button === 0){
+            if(e.target.classList.contains("piece")){
+                const mouseX = e.clientX - (e.target.clientWidth/2) + window.pageXOffset;
+                const mouseY = e.clientY - (e.target.clientHeight/2) + window.pageYOffset;
+                e.target.style.position = "absolute";
+                e.target.style.left = `${mouseX}px`;
+                e.target.style.top = `${mouseY}px`;
+                this.setState({
+                    grabbedPiece: e
+                })
+            }
         }
     }
 
     movePiece(e) {
         if(this.state.grabbedPiece){
-            const mouseX = e.clientX - (e.target.clientWidth/2);
-            const mouseY = e.clientY - (e.target.clientHeight/2);
-            let grabbedPiece = this.state.grabbedPiece;
+            const mouseX = e.clientX - (e.target.clientWidth/2) + window.pageXOffset;
+            const mouseY = e.clientY - (e.target.clientHeight/2) + window.pageYOffset;
+            console.log(e.target.parentNode.parentNode.clientHeight);
+            const grabbedPiece = this.state.grabbedPiece;
             grabbedPiece.target.style.left = `${mouseX}px`;
             grabbedPiece.target.style.top = `${mouseY}px`;
         }
@@ -47,6 +49,13 @@ class Chessboard extends React.Component {
         }
     }
 
+    handleRightClick(e){
+        e.preventDefault();
+        if(e.target.classList.contains("piece")){
+            e.target.parentNode.removeChild(e.target);
+        }
+    }
+
     render() { 
         let chessBoard = [];
         for(let i = 0; i < yAxis.length; i++){
@@ -54,7 +63,7 @@ class Chessboard extends React.Component {
                 chessBoard.push(<Tile key={`${j},${i}`} xAxis={j} yAxis={i}></Tile>)
             }
         }
-        return  <div className="chessboard-center"><div onMouseDown={this.grabPiece} onMouseMove={this.movePiece} onMouseUp={this.dropPiece} id="chessboard">{chessBoard}</div></div>;
+        return  <div className="chessboard-center"><div onMouseDown={this.grabPiece} onMouseMove={this.movePiece} onMouseUp={this.dropPiece} onContextMenu={this.handleRightClick} id="chessboard">{chessBoard}</div></div>;
     }
 }
  
